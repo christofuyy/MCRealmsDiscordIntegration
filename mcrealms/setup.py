a260d6fcb284db939610a06bfdd3e627974ffe56
Christopher <cyoun@chapman.edu>
2022-03-11 22:49:03 -0800

    chore(mcrealms): Configure mcrealms python package

---
+++ mcrealms/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='mcrealms',
+                 version='0.1',
+                 description='Minecraft Realms API Wrapper written in Python',
+                 url='',
+                 author='christofuyy',
+                 install_requires=['requests'],
+                 author_email='',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
